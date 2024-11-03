59c8170a0982502a0b99d8d891125d2754ac6c4b
Vedansh <superuser.ntsystems@outlook.com>
2024-11-03 12:09:18 +0530

    Enhanced Stuff.

--- src/MovieFinderApp.jsx
+++ src/MovieFinderApp.jsx
@@ -16,6 +16,9 @@ import {
   TopRatedSeries,
   UpcomingMovies,
   UpcomingSeries,
+  Contact,
+  About,
+  NotFound
 } from './pages';
 
 const router = createBrowserRouter([
@@ -73,32 +76,41 @@ const router = createBrowserRouter([
   },
   {
     path: '/*',
-    element: <Navigate to={'/'} />,
+    element: <NotFound />,
   },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/contact',
+    element: <Contact />,
+  }
 ]);
 
 export const MovieFinderApp = () => {
   return (
     <>
       <Helmet>
-        <meta property="og:type" content="website" />
-        <meta property="og:source" content="https://github.com/offensive-vk/Temp/" />
-        <meta property="og:site_name" content="TheExitMoviePlatform by Vedansh (offensive-vk)" />
-        <meta property="og:title" content="TheExitMoviePlatform - Find Everything, Anywhere, Anytime." />
-        <meta property="og:url" content="https://find-everything.pages.dev/" />
-        <meta property="og:dev" content="https://find-everything.pages.dev/" />
-        <meta property="og:image" content="/logo/logo-color.png"/>
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <meta charset="UTF-8" />
-        <meta http-equiv="content-language" content="en" />
-        <meta name="robots" content="index" data-robots="true"/>
-        <meta name="description" content="Discover popular movies and series with Movie Finder!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="TEMP - One Platform to Find Whatever You Need About Movies and Series." />
         <title>TheExitMoviePlatform - Find Everything, Anywhere, Anytime.</title>
         <link rel="canonical" href="https://find-everything.pages.dev/" />
         <link rel="shortcut icon" type="image/svg+xml" href="/logo/svg/logo-color.svg" />
         <link rel="icon" type="image/svg+xml" href="/logo/svg/logo-no-background.svg" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="TheExitMoviePlatform by Vedansh (offensive-vk)" />
+        <meta property="og:title" content="TheExitMoviePlatform - Find Everything, Anywhere, Anytime." />
+        <meta property="og:url" content="https://find-everything.pages.dev/" />
+        <meta property="og:dev" content="https://find-everything.pages.dev/about" />
+        <meta property="og:image" content="/logo/logo-color.png"/>
+        <meta property="og:source" content="https://github.com/offensive-vk/Temp/" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta charset="UTF-8" data-charset="UTF-8" />
+        <meta name="author" content="Vedansh (offensive-vk)" />
+        <meta http-equiv="content-language" content="en,es" />
+        <meta name="robots" content="index" data-robots="true"/>
+        <meta name="description" content="Discover popular movies and series to watch with Movie Finder!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="TEMP - One Platform to Find Whatever You Need About Movies and Series." />
         <meta name="keywords" content="temp, theexitmovieplatform, movie-finder, show-finder, movies, series, movie, series, search, find, discover, popular, upcoming, top-rated, on-the-air, genre, category, actor, actress, cast, crew, production, release, budget, revenue, rating, runtime, tagline, vote-count, seasons, episodes, last-air-date, production-companies, director, recommendations, watch-trailer, streaming, results, results-for, media-type, origin-country, cast, spoken-languages, status, release-date, budget, revenue, popularity, runtime, tagline, vote-count, seasons, episodes, last-air-date, production-companies, crew, recommendations, watch-trailer, streaming, results, results-for, media-type, origin-country, cast, spoken-languages, status, release-date, budget, revenue, popularity, runtime, tagline, vote-count, seasons, episodes, last-air-date, production-companies, crew, recommendations, watch-trailer, streaming" />
       </Helmet>
       <RouterProvider router={router} />
---
+++ src/pages/404/404.jsx
@@ -0,0 +1,5 @@
+import { Error } from "../../components";
+
+export const NotFound = () => {
+    return <Error />;
+};
